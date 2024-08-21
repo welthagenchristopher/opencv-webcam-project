95a8d6ce9a319c621d9f337c6d3cd507a1227f71
welthagenchristopher <welthagen.christopher@gmail.com>
2024-08-21 17:46:53 +1200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)  # use 0 for web camera
+camera = cv2.VideoCapture(0)  # use 0 for device webcam
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv2.VideoCapture(0)
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -22,7 +21,7 @@ def gen_frames():  # generate frame by frame from camera
 
 @app.route('/video_feed')
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
+    #Video streaming route
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
